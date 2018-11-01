1618a043fb1d6d1efae4053d5e802eb86cfce1ae
lettda <lettda@vcu.edu>
2018-11-01 10:41:22 -0400

    Eureka a palendrome!
    fixed the reverse string function to make reversedInput contain the proper characters

--- Palendrone/Palendrone/main.cpp
+++ Palendrone/Palendrone/main.cpp
@@ -8,37 +8,31 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <cstring>
 using namespace std;
 void reverseString(char arr[100]);
 char reversedInput[100];
 
 
 int main() {
-    
+
     char input[100];
-    
-    
+
+
     cout<<"Please enter a string"<<endl;
     cin.getline(input, 100);
     cout<<"You entered "<<input<<endl;
-    
+
     reverseString(input);
     cout<<"The reverse is "<<reversedInput<<endl;
-    
+
     system("pause");
     return 0;
 }
 
-void reverseString (char arr[]) {
-    int i;
-    int j = strlen(arr) - 1;
-    char temp;
-    
-    for (i = 0; i < j; i++, j--) {
-        temp = arr[j];
-        //        arr[j];
-        reversedInput[i] = temp;
-    }
-    
+void reverseString(char arr[]) 
+{
+    size_t len = strlen(arr);
+    for (size_t i = 0; len-- > 0; ++i)
+        reversedInput[i] = arr[len];
 }
-
